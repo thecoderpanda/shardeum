e203754286fff3705535ccc91fdd5d66226704d5
Shantanu <shantanu@thecoderpanda.com>
2023-07-27 18:32:18 +0530

    added files

---
+++ contracts/TokenA.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TokenA is ERC20 {
+
+  constructor(uint256 initialSupply) ERC20("TokenA", "TokA") {
+      _mint(msg.sender, initialSupply);
+  }
+
+}
